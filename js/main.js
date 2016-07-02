4c7b6e1017edc4dc8c0132a3ad3e03ac3e67e9bb
Daniel Duazo <danielduazo@berkeley.edu>
2016-07-01 19:10:24 -0700

    Updated resume, about section. July 2016

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function get_email_link (link_title) {
-	var username = "aaron";
-	var hostname = "agundy.com";
-	var link = username + "@" + hostname;
-	link_title = link_title || link;
-	document.write("<a href='" + "mail" + "to:" + link + "''>"+link_title + "</a>" )
-}
